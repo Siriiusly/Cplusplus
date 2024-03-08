4e558ad39a0cfc2203981f79ef6fd11cfd36ca7c
Siriiusly <162720054+Siriiusly@users.noreply.github.com>
2024-03-08 19:38:18 +0300

    Add files via upload

---
+++ Bazı kurallar.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Deðiþken isimlerinde bazý kurallar:
+	// -Deðiþken isimlerinde ilk karakter rakam olamaz.
+    // -Deðiþken isimlerinde Türkçe karakter kullanma.
+	// -Deðiþken isimlerinde arada boþkuk olamaz.
+	// -Deðiþken isimlerinde arada nokta vb. karakter kullanýlmaz.
+
+	//Deðiþken isimlerini camelCase yapýsýnda yazmanýz tavsiye olunur.
+	int dogumYili = 2004;
+	int birinciSinavNotu = 98;
+
+
+	
+
+	return 0;
+
+
+
+
+
+
+}
