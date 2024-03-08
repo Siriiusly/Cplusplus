16f4ec0a79a75acf9dad5a979b9939a15a41ce5c
Siriiusly <162720054+Siriiusly@users.noreply.github.com>
2024-03-08 20:02:20 +0300

    Add files via upload

---
+++ Değişkenlerle ilgili örnekler 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string isim = "Merve";
+	int dogumYili = 2007;
+	float sayisalNotOrtalamasi = 3.47f;
+	cout << "Merhaba sayin " << isim << endl;
+	cout << "Dogum Yili: " << dogumYili << endl;
+	cout << "Not Ortalamasi: " << sayisalNotOrtalamasi << endl;
+
+	
+
+	return 0;
+
+
+
+
+
+
+}
