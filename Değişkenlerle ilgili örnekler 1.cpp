bb1c131f4a3af793d9557b69ab82da781d31f2e5
Siriiusly <162720054+Siriiusly@users.noreply.github.com>
2024-03-08 20:12:39 +0300

    Update Değişkenlerle ilgili örnekler 1.cpp

--- Değişkenlerle ilgili örnekler 1.cpp
+++ Değişkenlerle ilgili örnekler 1.cpp
@@ -7,9 +7,14 @@ int main()
 	string isim = "Merve";
 	int dogumYili = 2007;
 	float sayisalNotOrtalamasi = 3.47f;
+        bool sinifiGectiMi = true;
+        char notOrtalamasiHarf = 65;   //ASCII tablosundan faydanılabilir char veri tipinde
+	
 	cout << "Merhaba sayin " << isim << endl;
 	cout << "Dogum Yili: " << dogumYili << endl;
 	cout << "Not Ortalamasi: " << sayisalNotOrtalamasi << endl;
+	cout << "Sinifi Gectiniz mi: " << sinifiGectiMi << endl;
+        cout << "Not ortalamasi Harf: " << notOrtalamasiHarf << endl;
 
 	
 
