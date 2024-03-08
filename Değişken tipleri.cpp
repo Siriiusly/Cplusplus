38b3ae65746c9ac7ddfaf1a32d2d8d7039ca2215
Siriiusly <162720054+Siriiusly@users.noreply.github.com>
2024-03-08 18:56:05 +0300

    Add files via upload

---
+++ Değişken tipleri.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string isim; //tanimlama
+	isim = "Tarik Bugra"; //atama
+
+
+	cout << "Bir zamanlar Merve isminde bir kiz varmis\n";
+	cout << "Merve " <<isim<<" cok severmis\n";
+		cout << isim<<" da merveyi cok severmis\n";
+	cout << "Birbirlerine tanidiklari zaman onlara yetmezmis\n";
+	cout << "Evlenmisler ve zamanlarini hep beraber gecirmeye karar vermisler\n";
+
+	return 0;
+
+
+
+
+
+
+}
