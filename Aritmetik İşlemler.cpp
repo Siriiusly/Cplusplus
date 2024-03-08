ea2f44b5e1c39f17cdd91f6dab5a9410d1cf6508
Siriiusly <162720054+Siriiusly@users.noreply.github.com>
2024-03-08 20:34:35 +0300

    Add files via upload

---
+++ Aritmetik İşlemler.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    /*
+	string isim = "Merve";
+	int dogumYili = 2007;
+	float sayisalNotOrtalamasi = 3.47f;
+	bool sinifiGectiMi = true;
+	char notOrtalamasiHarf = 65;   //ASCII tablosundan faydanýlabilir char veri tipinde
+	float quizNot1 = 70.f;
+	int quizNot2 = 85;
+	//float quizOrtalama = (float) (quizNot1 + quizNot2) / 2;
+	float quizOrtalama = (quizNot1 + quizNot2) / 2;
+
+
+
+	cout << "Merhaba sayin " << isim << endl;
+	cout << "Dogum Yili: " << dogumYili << endl;
+	cout << "Not Ortalamasi: " << sayisalNotOrtalamasi << endl;
+	cout << "Sinifi Gectiniz mi: " << sinifiGectiMi << endl;
+	cout << "Not ortalamasi Harf: " << notOrtalamasiHarf << endl;
+	cout << "Quiz Not Ortalamasi: " << quizOrtalama  << endl;
+	*/
+	int x = 30;
+	int y = 20;    // % mod(modulus) iþareti
+	int z = 15 % 2;
+	cout << z << endl;
+
+	
+
+	return 0;
+
+
+
+
+
+
+}
