d2c4e9cbb93c81c01f750de2c1aed2653161a15d
Siriiusly <162720054+Siriiusly@users.noreply.github.com>
2024-03-11 20:36:15 +0300

    Add files via upload

---
+++ İf Else örnek 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int sicaklik;
+	cout << "Sicakligi giriniz: ";
+	cin >> sicaklik;
+
+	if (sicaklik>=35) {
+		cout << "Ortam Sicak!\n";
+	}
+	else
+	{
+		cout << "Ortam sicak degil\n";
+	}
+
+
+
+
+
+
+
+
+	return 0;
+}
